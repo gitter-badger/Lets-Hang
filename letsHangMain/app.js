da0e3e1bb2a84cd9d7c524b9687aa607c4ab35df
Christian Grabowki <christiang11754@gmail.com>
2014-10-05 16:55:49 -0400

    move to express 4

--- letsHangMain/app.js
+++ letsHangMain/app.js
@@ -58,22 +58,20 @@ var googleKey = 'AIzaSyBfIApUobHr1J1OYNpBIy9D1AL5cfZadgs';
 //************************SETTINGS*************************
 //*********************************************************
 
-app.configure(function () {
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'hbs');
-  app.engine('hbs', hbs.express3({
-    partialsDir: __dirname + '/views/partials'
-  }));
-  app.use(express.static(path.join(__dirname, 'public')));
-  app.use(express.json());       // to support JSON-encoded bodies
-  app.use(express.urlencoded()); // to support URL-encoded bodies
-  app.use(morgan('dev'));
-  app.use(cookieParser());
-  app.use(bodyParser());
-  app.use(session({secret:'B1_1ubbadoo!?'}));
-  app.use(passport.initialize());
-  app.use(passport.session());
-});
+app.set('views', __dirname + '/views');
+app.set('view engine', 'hbs');
+app.engine('hbs', hbs.express3({
+  partialsDir: __dirname + '/views/partials'
+}));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());       // to support JSON-encoded bodies
+app.use(express.urlencoded()); // to support URL-encoded bodies
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(bodyParser());
+app.use(session({secret:'B1_1ubbadoo!?'}));
+app.use(passport.initialize());
+app.use(passport.session());
 io.set('authorization', function (handshakeData, callback) {
   callback(null, true); // error first callback style 
 });
