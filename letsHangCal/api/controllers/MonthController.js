e8f73e2a20b56bf53ea5af1886b8ce6d3e8f099d
Christian Grabowski <christiang11754@gmail.com>
2014-11-07 00:07:47 -0500

    calendar work

--- letsHangCal/api/controllers/MonthController.js
+++ letsHangCal/api/controllers/MonthController.js
@@ -6,6 +6,23 @@
  */
 
 module.exports = {
-	
+	getAllActivities: function(user, month){
+		var acts = [];
+		var send = false;
+		Activity.find({creator: user.id}).exec(function(err, cAct){
+			acts.push(cActs);
+			Activity.find({invited: user.id}).exec(function(err, iAct){
+				acts.push(iActs);
+				for(var i = 0; i<acts.length; i++){
+					if(acts[i].startDate.getMonth()!=month){
+						acts.splice(i, 1);
+					}
+				}
+				send = true;
+			});
+		});
+		while(!send){}
+		return acts;
+	}
 };
 
--- letsHangCal/api/controllers/YearController.js
+++ letsHangCal/api/controllers/YearController.js
@@ -6,6 +6,32 @@
  */
 
 module.exports = {
-	
+	getAllActivities: function(user){
+		var acts = [];
+		var send = false;
+		Activity.find({creator: user.id}, function(err, cAct){
+			if(err){
+				return user;
+			}
+			acts.push(cAct);
+			Activity.find({invited: user.id}).exec(function(err, iAct){
+				acts.push(iAct);
+				for(var i =0; i<acts.length; i++){
+					if(acts[i].startDate.getFullYear() > new Date().getFullYear()){
+						acts.splice(i, 1);
+					}
+					if(i == acts.length-1){
+						send = true;
+					}
+				}
+				if(send){
+					res.json(acts);
+				}
+				else{
+					setTimeout(200);
+				}
+			});
+		});
+	}
 };
 
